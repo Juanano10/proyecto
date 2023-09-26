08b4106de67c0884c4b9c5fb9518cc8fea8aa1ec
Juanano10 <jua.bustosl@duocuc.cl>
2023-09-26 02:53:32 -0300

    26/09/2023 inicio

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Navbar() {
     const inactiveLink = 'flex p-4 gap-5';
-    const activeLink = inactiveLink+'bg-white text-blue-900 rounded-l-lg';
+    const activeLink = inactiveLink + 'bg-white text-blue-900 rounded-l-lg';
 
     return (
         <aside className="text-white p-2 pr-0">
@@ -17,6 +17,19 @@ function Navbar() {
             <nav className="flex flex-col gap-5">
                 <Link href={"/informes"} className={inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+
+
+                    <span className="">
+                        Perfil
+                    </span>
+                    <span>
+
+                    </span>
+                </Link>
+                <Link href={"/informes"} className={inactiveLink}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                     </svg>
 
@@ -24,7 +37,7 @@ function Navbar() {
                         Informes
                     </span>
                     <span>
-                        
+
                     </span>
                 </Link>
                 <Link href={"/inventario"} className={inactiveLink}>
@@ -37,24 +50,24 @@ function Navbar() {
                     </span>
                 </Link>
                 <Link href={"/analisis"} className={inactiveLink}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
-                </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
+                    </svg>
                     <span className="">
                         Analisis
                     </span>
                 </Link>
 
                 <Link href={"/historial"} className={inactiveLink}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                     </svg>
 
                     <span className="">
                         Historial de movimientos
                     </span>
 
-                
+
                 </Link>
                 <Link href={"/"} className={inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -65,7 +78,7 @@ function Navbar() {
                         Cerrar sesion
                     </span>
 
-                
+
                 </Link>
             </nav>
         </aside>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,70 +1,45 @@
-import React from 'react';
-import Navbar from 'src/app/components/Navbar';
-import Footer from '@/components/Footer'; 
 
-function HomePage() {
-  const movements = [
-    { id: 1, date: '2023-09-25', description: 'Venta', product: 'Producto A', quantity: 5, type: 'Decremento' },
-    { id: 2, date: '2023-09-24', description: 'Compra', product: 'Producto B', quantity: 10, type: 'Incremento' },
-    // Agregar más movimientos según sea necesario
-  ];
-
+import Link from "next/link"
+function LoginPage() {
   return (
-    <div className="bg-indigo-600 min-h-screen flex flex-col">
-      <div className="flex-grow flex">
-        <Navbar />
-        <div className="bg-white flex-grow mt-1 mr-2 mb-2 rounded-lg p-4">
-          <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Sistema de Inventario</h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-500">
+      <div className="bg-zinc-700 rounded-md p-10 w-96">
+        <form className="space-y-4">
+          <h3 className="text-2xl font-bold text-white text-center">Inicio de Sesión</h3>
+          <div>
+            
 
-            {/* Búsqueda y Filtrado */}
-            <div className="mb-4">
-              <input
-                className="border border-gray-400 py-2 px-4 w-full rounded-md"
-                type="text"
-                placeholder="Buscar productos"
-              />
-            </div>
-
-            {/* Lista de Productos con Búsqueda y Filtrado */}
-            <div className="bg-white p-4 shadow-md rounded">
-              <h2 className="text-xl font-semibold mb-2">Lista de Productos</h2>
-              {/* Aquí puedes mostrar la lista de productos con capacidades de búsqueda y filtrado */}
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th>Producto</th>
-                    <th>Stock Disponible</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {movements.map((movement) => (
-                    <tr key={movement.id}>
-                      <td>{movement.product}</td>
-                      <td>{movement.quantity}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-
-            {/* Control de Stock */}
-            <div className="bg-white p-4 shadow-md rounded mt-4">
-              <h2 className="text-xl font-semibold mb-2">Control de Stock</h2>
-              {/* Aquí puedes agregar controles para ajustar el stock de productos */}
-            </div>
-
-            {/* Generación de Informes */}
-            <div className="bg-white p-4 shadow-md rounded mt-4">
-              <h2 className="text-xl font-semibold mb-2">Generación de Informes</h2>
-              {/* Aquí puedes incluir opciones para generar informes */}
-            </div>
           </div>
-        </div>
+          <div>
+            <input
+              className="bg-zinc-700 px-4 py-2 w-full text-white placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+              type="email"
+              placeholder="Correo Electrónico"
+              name="email"
+            />
+          </div>
+          <div>
+            <input
+              className="bg-zinc-700 px-4 py-2 w-full text-white placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+            />
+          </div>
+          <div>
+          <Link href={"/home"} >
+            <button
+              className="bg-indigo-500 text-white px-5 py-2 w-full rounded-md hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-700"
+              type="submit"
+            >
+              Iniciar Sesión
+            </button>
+            </Link>
+          </div>
+        </form>
       </div>
-      <Footer />
     </div>
   );
-}
+  }
 
-export default HomePage;
+export default LoginPage
