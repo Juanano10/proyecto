94aeff1bc4085d25361c5e8df6ae69c06a6f1e38
Juanano10 <jua.bustosl@duocuc.cl>
2023-09-26 03:48:09 -0300

    1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      
       <body >{children}</body>
     </html>
   )
