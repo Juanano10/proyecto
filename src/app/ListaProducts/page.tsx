d1d7dfb1d823dd5027614e2c15051c7aeda946ee
LivanAraya22 <117540165+LivanAraya22@users.noreply.github.com>
2023-11-13 00:11:03 -0300

    Cambios Dashboar /animacion

--- src/app/ListaProducts/page.tsx
+++ src/app/ListaProducts/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ReactLoading from "react-loading";
 
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
@@ -13,19 +14,26 @@ import ReactPaginate from "react-paginate";
 function HomePage() {
   const [products, setProducts] = useState([]);
   const [searchText, setSearchText] = useState("");
-  const [currentPage, setCurrentPage] = useState(0); // Cambiamos el valor inicial a 0
+  const [currentPage, setCurrentPage] = useState(0);
+  const [loading, setLoading] = useState(true);
   const productsPerPage = 10;
 
   useEffect(() => {
-    // Realiza una solicitud GET a tu punto final de la API para obtener la lista de productos
-    axios
-      .get("/api/product/id")
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/product/id");
         setProducts(response.data.products);
-      })
-      .catch((error) => {
+
+        // Simular una carga de 2 segundos
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      } catch (error) {
         console.error("Error al obtener la lista de productos", error);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
 
   const handlePageClick = ({ selected }) => {
@@ -49,7 +57,7 @@ function HomePage() {
         <Navbar />
         <div className="mb-4"></div>
         <div className="mb-4"></div>
-        <div className="bg-blue-900 flex-grow mr-2 mb-1 rounded-lg p-4 mt-1 max-w-[sm]">
+        <div className="bg-gradient-to-r from-blue-700 via-indigo-500 to-blue-900 flex-grow mr-2 mb-1 rounded-lg p-4 mt-1 max-w-[sm]">
           <div className="container mx-auto p-1 mt-3">
             <div className="flex items-center justify-between mb-4">
               <h1 className="text-3xl font-bold flex-grow text-white">
@@ -74,62 +82,123 @@ function HomePage() {
               <h2 className="text-lg sm:text-2xl font-semibold text-center text-black py-2 mb-4 border-b-2 border-black">
                 Lista de Productos
               </h2>
-              <table className="w-full border border-black">
-                <thead className="bg-gray-200 border-b border-black">
-                  <tr>
-                    <th className="py-2 px-4 text-left">Nombre</th>
-                    <th className="py-2 px-4 text-center">Precio</th>
-                    <th className="py-2 px-4 text-left">Descripción</th>
-                    <th className="py-2 px-4 text-center">Código</th>
-                    <th className="py-2 px-4 text-center">Stock</th>
-                    <th className="py-2 px-4 text-center">Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {currentProducts.map((product) => (
-                    <tr key={product._id}>
-                <td className="border-r border-black py-2 px-4 text-left">
-  <div className="text-gray-900 font-semibold" style={{ maxWidth: "150px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
-    {product.name}
-  </div>
-</td>
-<td className="border-r border-black py-2 px-4 text-left">
-  <div className="text-gray-900 font-semibold" style={{ maxWidth: "100px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
-    {product.price}
-  </div>
-</td>
-<td className="border-r border-black py-2 px-4 text-left">
-  <div className="text-gray-900 font-semibold" style={{ maxWidth: "200px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
-    {product.description}
-  </div>
-</td>
-<td className="border-r border-black py-2 px-4 text-left">
-  <div className="text-gray-900 font-semibold" style={{ maxWidth: "100px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
-    {product.code}
-  </div>
-</td>
-<td className="border-r border-black py-2 px-4 text-left">
-  <div className="text-gray-900 font-semibold" style={{ maxWidth: "100px", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis" }}>
-    {product.stock}
-  </div>
-</td>
-                      <td className="flex justify-center">
-                        <button className="mx-2"></button>
-                        <button className="mx-2">
-                          <UpdateBtn
-                            id={product._id}
-                            name={product.name}
-                            email={product.price}
-                            role={product.description}
-                            password={product.code}
-                            password={product.stock}
-                          />
-                        </button>
-                      </td>
+
+              {loading ? (
+                <div className="flex justify-center items-center h-4/5">
+                  <div className="bg-gray-200 p-8 rounded-lg shadow-lg w-4/5 h-4/5 flex flex-col items-center mx-auto my-auto">
+                    <ReactLoading
+                      type={"spin"}
+                      color={"#3182ce"}
+                      height={200}
+                      width={200}
+                    />
+                    <p className="mt-4 text-black-800 text-lg font-bold animate-pulse">
+                      Cargando productos
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                // Tu contenido cuando no está cargando
+
+                // Tu contenido cuando no está cargando
+
+                <table className="w-full border border-black">
+                  <thead className="bg-gray-200 border-b border-black">
+                    <tr>
+                      <th className="py-2 px-4 text-left">Nombre</th>
+                      <th className="py-2 px-4 text-center">Precio</th>
+                      <th className="py-2 px-4 text-left">Descripción</th>
+                      <th className="py-2 px-4 text-center">Código</th>
+                      <th className="py-2 px-4 text-center">Stock</th>
+                      <th className="py-2 px-4 text-center">Acciones</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {currentProducts.map((product) => (
+                      <tr key={product._id}>
+                        <td className="border-r border-black py-2 px-4 text-left">
+                          <div
+                            className="text-gray-900 font-semibold"
+                            style={{
+                              maxWidth: "150px",
+                              overflow: "hidden",
+                              whiteSpace: "nowrap",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            {product.name}
+                          </div>
+                        </td>
+                        <td className="border-r border-black py-2 px-4 text-left">
+                          <div
+                            className="text-gray-900 font-semibold"
+                            style={{
+                              maxWidth: "100px",
+                              overflow: "hidden",
+                              whiteSpace: "nowrap",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            {product.price}
+                          </div>
+                        </td>
+                        <td className="border-r border-black py-2 px-4 text-left">
+                          <div
+                            className="text-gray-900 font-semibold"
+                            style={{
+                              maxWidth: "200px",
+                              overflow: "hidden",
+                              whiteSpace: "nowrap",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            {product.description}
+                          </div>
+                        </td>
+                        <td className="border-r border-black py-2 px-4 text-left">
+                          <div
+                            className="text-gray-900 font-semibold"
+                            style={{
+                              maxWidth: "100px",
+                              overflow: "hidden",
+                              whiteSpace: "nowrap",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            {product.code}
+                          </div>
+                        </td>
+                        <td className="border-r border-black py-2 px-4 text-left">
+                          <div
+                            className="text-gray-900 font-semibold"
+                            style={{
+                              maxWidth: "100px",
+                              overflow: "hidden",
+                              whiteSpace: "nowrap",
+                              textOverflow: "ellipsis",
+                            }}
+                          >
+                            {product.stock}
+                          </div>
+                        </td>
+                        <td className="flex justify-center">
+                          <button className="mx-2"></button>
+                          <button className="mx-2">
+                            <UpdateBtn
+                              id={product._id}
+                              name={product.name}
+                              email={product.price}
+                              role={product.description}
+                              password={product.code}
+                              password={product.stock}
+                            />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
               <div className="mt-4">
                 <ReactPaginate
                   previousLabel={"Anterior"}
@@ -151,7 +220,6 @@ function HomePage() {
                     "bg-white text-black p-2 rounded border border-black hover:bg-blue-500 hover:text-white transform transition-transform duration-200 ease-in-out hover:scale-105"
                   }
                   disabledClassName={"opacity-50 cursor-not-allowed"}
-                  // Estilos para los números de página
                   pageLinkClassName={
                     "bg-white text-black p-2 rounded border-black hover:bg-blue-500 hover:text-white transform transition-transform duration-200 ease-in-out hover:scale-105"
                   }
--- src/app/historial/page.tsx
+++ src/app/historial/page.tsx
@@ -3,16 +3,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import ReactPaginate from "react-paginate";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { InventoryTransactionDocument } from "../../models/InventarioTrans";
 
 function Historial() {
-  const [inventariotrans, setInventarioTrans] = useState<InventoryTransactionDocument[]>([]);
+  const [inventariotrans, setInventarioTrans] = useState<
+    InventoryTransactionDocument[]
+  >([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 10; // Cambia el número de elementos por página según tus necesidades
 
   useEffect(() => {
-    // Cambia la ruta de la solicitud a la correcta
+    // Realiza la solicitud de datos a la API
     axios
       .get("/api/InventoryTransaction/id")
       .then((response) => {
@@ -23,37 +28,107 @@ function Historial() {
       });
   }, []);
 
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedItems = inventariotrans.slice(startIndex, endIndex);
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col">
       <Header />
       <div className="flex-grow flex">
         <Navbar />
-        <div className="bg-white flex-grow mt-1 mr-2 mb-2 rounded-lg p-4">
+        <div className="bg-gradient-to-r from-blue-700 via-indigo-500 to-blue-900 flex-grow mr-2 mb-1 rounded-lg p-4 mt-1 max-w-[sm]">
           <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Historial de Movimientos</h1>
+            <h1 className="text-3xl font-bold flex-grow text-white">Historial de Movimientos</h1>
             <div className="overflow-x-auto">
-              <table className="w-full table-auto">
-                <thead>
-                  <tr>
-                    <th className="border px-4 py-2">Fecha</th>
-                    <th className="border px-4 py-2">Producto</th>
-                    <th className="border px-4 py-2">Cantidad</th>
-                    <th className="border px-4 py-2">Tipo</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {inventariotrans.map((trans) => (
-                    <tr key={trans._id}>
-                      <td className="border px-4 py-2">{new Date(trans.timestamp).toLocaleDateString()}</td>
-                      <td className="border px-4 py-2">{trans.product /* Si product es un ObjectId, se necesita más información para mostrarlo adecuadamente */}</td>
-                      <td className="border px-4 py-2">{trans.stock}</td>
-                      <td className={`border px-4 py-2 ${trans.type === 'entrada' ? 'text-green-600' : 'text-red-600'}`}>
-                        {trans.type === 'entrada' ? 'Incremento' : 'Decremento'}
-                      </td>
+              <h2 className="text-lg sm:text-2xl font-semibold text-center text-white py-2 mb-4 border-b-2 border-black">
+                Lista de movimientos de productos
+              </h2>
+              <div className="shadow-md overflow-hidden border border-black rounded-lg">
+                <table className="w-full table-auto">
+                  <thead>
+                    <tr className="bg-black text-white">
+                      <th className="border border-black px-4 py-2">Fecha</th>
+                      <th className="border border-black px-4 py-2">Producto</th>
+                      <th className="border border-black px-4 py-2">Cantidad</th>
+                      <th className="border border-black px-4 py-2">Tipo</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {/* Mapear y mostrar los movimientos de productos */}
+                    {displayedItems.map((trans) => (
+                      <tr key={trans._id} className="bg-white text-black">
+                        <td className="border border-black px-4 py-2">
+                          {new Date(trans.timestamp).toLocaleDateString()}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {trans.product /* Asegúrate de que trans.product sea legible */}
+                        </td>
+                        <td className="border border-black px-4 py-2">{trans.stock}</td>
+                        <td
+                          className={`border border-black px-4 py-2 text-center ${
+                            trans.type === 'entrada' ? 'text-green-600 font-semibold' : 'text-red-600 font-semibold'
+                          }`}
+                        >
+                          {trans.type === 'entrada' ? (
+                            <div className="flex items-center justify-center">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 text-green-600 mr-1 transform rotate-180"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                {/* Ícono para entrada */}
+                              </svg>
+                              Incremento
+                            </div>
+                          ) : (
+                            <div className="flex items-center justify-center">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 text-red-600 mr-1"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                {/* Ícono para salida */}
+                              </svg>
+                              Decremento
+                            </div>
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="mt-4">
+              {/* Paginación */}
+              <ReactPaginate
+                previousLabel={"Anterior"}
+                nextLabel={"Siguiente"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={Math.ceil(inventariotrans.length / itemsPerPage)}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageChange}
+                containerClassName="flex justify-center mt-4"
+                pageClassName="mx-2"
+                previousLinkClassName={
+                  "bg-white text-black p-2 rounded border border-black hover:bg-blue-500 hover:text-white transform transition-transform duration-200 ease-in-out hover:scale-105"
+                }
+                nextLinkClassName={
+                  "bg-white text-black p-2 rounded border border-black hover:bg-blue-500 hover:text-white transform transition-transform duration-200 ease-in-out hover:scale-105"
+                }
+                disabledClassName={"opacity-50 cursor-not-allowed"}
+                pageLinkClassName={
+                  "bg-white text-black p-2 rounded border border-black hover:bg-blue-500 hover:text-white transform transition-transform duration-200 ease-in-out hover:scale-105"
+                }
+              />
             </div>
           </div>
         </div>
@@ -61,6 +136,8 @@ function Historial() {
       <Footer />
     </div>
   );
+  
+  
 }
 
 export default Historial;
