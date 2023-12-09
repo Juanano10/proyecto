9966bdb133dbb7095332e0737eefc6d815ba59fc
EstebanSalazarLe <145887836+EstebanSalazarLe@users.noreply.github.com>
2023-12-08 22:26:44 -0300

    ugu

--- src/app/inventario/page.tsx
+++ src/app/inventario/page.tsx
@@ -1,227 +1,86 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-"use client";
-import React, { FormEvent, useState } from "react";
-import axios, { AxiosError } from "axios";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+// ... Otras importaciones ...
 
-function inventario() {
-  const [error, setError] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+import { useSession } from "next-auth/react";
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+function Inventario() {
+  const { data: session } = useSession();
+  const isAdmin = () => session?.user?.role === "administrador";
 
-    const name = formData.get("name") as string;
-    const price = parseFloat(formData.get("price") as string); // Parse price as a number
-    const description = formData.get("description") as string;
-    const code = parseFloat(formData.get("code") as string);
-    const cost = parseFloat(formData.get("cost") as string);
-    const stock = parseFloat(formData.get("stock") as string);
-    const category =formData.get("category") as string;
-
-
-    if (
-      !name.trim() ||
-      isNaN(price) ||
-      isNaN(code) ||
-      isNaN(stock) ||
-      !description.trim()
-    ) {
-      setError("Por favor, completa todos los campos correctamente.");
-      return;
-    }
-
-    try {
-      const response = await axios.post("/api/product/id", {
-        name: name,
-        price: price,
-        description: description,
-        code: code,
-        cost: cost,
-        category: category,
-        stock: stock,
-      });
-
-      console.log(response);
-      setSuccessMessage("¡Producto registrado con éxito!");
-      setError(null);
-    } catch (error) {
-      console.log(error);
-      if (error instanceof AxiosError) {
-        setError(error.response?.data.message);
-      }
-      setSuccessMessage(null);
-    }
-  };
+  // Resto de tu código...
 
   return (
     <div className="bg-gray-900 min-h-screen flex flex-col">
       <Header />
       <div className="flex-grow flex">
         <Navbar />
-
         <div className="bg-blue-900 flex-grow mr-2 mb-1 rounded-lg p-4 mt-1 max-w-[sm]">
+          {/* ... Otro contenido del componente ... */}
           <div className="container mx-auto p-1 mt-3">
             <div className="card">
-              <form
-                className="w-full max-w-md mx-auto p-6 bg-gray-800 text-white rounded shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
-                onSubmit={handleSubmit}
-              >
-                {error && (
-                  <div className="bg-slate-400 border-black border-4 text-black p-2 mb-2">
-                    {error}
-                  </div>
-                )}
-                {successMessage && (
-                  <div className="bg-green-400 border-green-700 border-4 text-black p-2 mb-2">
-                    {successMessage}
-                  </div>
-                )}
-
-                <h3 className="text-2xl font-bold text-center mb-6">
-                  <u className="hover:underline hover:cursor-pointer transition duration-300 ease-in-out">
-                    Registrar Producto
-                  </u>
-                </h3>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="name"
+              {/* ... Resto del formulario ... */}
+              <h3 className="text-2xl font-bold text-center mb-6">
+                <u className="hover:underline hover:cursor-pointer transition duration-300 ease-in-out">
+                  Registrar Producto
+                </u>
+              </h3>
+              {isAdmin() && (
+                // Contenido exclusivo para administradores
+                <div>
+                  <h4 className="text-xl font-bold mb-2 text-center">
+                    Agregar Usuario
+                  </h4>
+                  <form
+                    // ... Ajusta los atributos del formulario según sea necesario
                   >
-                    Nombre del Producto
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Nombre del Producto"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="name"
-                  >
-                    Categoria del Producto
-                  </label>
-                  <select
-  id="category"
-  name="category"
-  className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
->
-  <option value="" disabled selected>
-    Selecciona una categoría
-  </option>
-  <option value="zapatillas">Zapatillas</option>
-  <option value="camisetas">Camisetas</option>
-  <option value="pantalones">Pantalones</option>
-  <option value="accesorios">Accesorios</option>
-  {/* Agrega más opciones según sea necesario */}
-</select>
+                    {/* Agrega los campos necesarios para agregar un usuario */}
+                    {/* Por ejemplo: nombre, correo electrónico, rol, etc. */}
+                    <div className="mb-4">
+                      <label
+                        className="block text-sm font-semibold mb-2 text-gray-300"
+                        htmlFor="userName"
+                      >
+                        Nombre de Usuario
+                      </label>
+                      <input
+                        type="text"
+                        id="userName"
+                        name="userName"
+                        placeholder="Nombre de Usuario"
+                        className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
+                      />
+                    </div>
 
+                    {/* Agrega más campos según sea necesario */}
+                    
+                    <div className="mb-6">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-400 text-white font-semibold py-2 px-4 rounded border border-blue-600 hover:border-blue-400 focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
+                        Agregar Usuario
+                      </button>
+                    </div>
+                  </form>
                 </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="price"
-                  >
-                    Precio
-                  </label>
-                  <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    placeholder="Precio"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="price"
-                  >
-                    Costo
-                  </label>
-                  <input
-                    type="number"
-                    id="cost"
-                    name="cost"
-                    placeholder="Costo"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="description"
-                  >
-                    Descripción
-                  </label>
-                  <input
-                    type="text"
-                    id="description"
-                    name="description"
-                    placeholder="Descripción"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="code"
-                  >
-                    Nombre del código
-                  </label>
-                  <input
-                    type="text"
-                    id="code"
-                    name="code"
-                    placeholder="Nombre del código"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="stock"
-                  >
-                    Cantidad de stock
-                  </label>
-                  <input
-                    type="number"
-                    id="stock"
-                    name="stock"
-                    placeholder="Cantidad de stock"
-                    className="w-full px-4 py-2 rounded border border-gray-800 text-gray-800 focus:outline-none focus:shadow-outline"
-                  />
-                </div>
-
-                <div className="mb-6">
-                  <button
-                    className="bg-red-500 hover:bg-red-400 text-white font-semibold py-2 px-4 rounded border border-red-600 hover:border-red-400 focus:outline-none focus:shadow-outline"
-                    type="submit"
-                  >
-                    Registrar
-                  </button>
-                </div>
-              </form>
+              )}
+              {/* ... Resto del código del formulario ... */}
             </div>
           </div>
         </div>
       </div>
       <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        progress={undefined}
+        style={{ fontSize: '1.4rem' }}
+      />
     </div>
   );
 }
 
-export default inventario;
+export default Inventario;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,25 +1,35 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 import axios, { AxiosError } from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { useSession } from "next-auth/react";
 
 function RegisterPage() {
+  const { data: session } = useSession();
+  const formRef = useRef<HTMLFormElement>(null);
   const [error, setError] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
 
+    if (session?.user?.role !== "administrador") {
+      toast.error("No tienes los permisos para registrar usuarios.");
+      return;
+    }
+
+    const formData = new FormData(e.currentTarget);
     const name = formData.get("name") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    const role = formData.get("role") as UserRoles; // Nuevo campo "role"
+    const role = formData.get("role") as string;
 
     if (!name.trim() || !email.trim() || !password.trim() || !role) {
-      setError("Por favor, rellena todos los campos.");
+      toast.error("Por favor, rellena todos los campos.");
       return;
     }
 
@@ -28,18 +38,25 @@ function RegisterPage() {
         email,
         password,
         name,
-        role, // Pasamos el campo "role" al backend
+        role,
       });
       console.log(res);
-      setSuccessMessage("¡Registro completado con éxito!");
-      setError(null);
+      toast.success("¡Registro completado con éxito!");
+      resetForm();
     } catch (error) {
       console.log(error);
       if (error instanceof AxiosError) {
-        setError(error.response?.data.message);
+        toast.error(error.response?.data.message || "Ha ocurrido un error");
+        setError(error.response?.data.message || "Ha ocurrido un error");
       }
-      setSuccessMessage(null);
     }
+  };
+
+  const resetForm = () => {
+    if (formRef && formRef.current) {
+      formRef.current.reset();
+    }
+    setError(null);
   };
 
   return (
@@ -50,107 +67,125 @@ function RegisterPage() {
         <div className="bg-blue-900 flex-grow mr-2 mb-1 rounded-lg p-4 mt-1 max-w-[sm]">
           <div className="container mx-auto p-1 mt-3">
             <div className="card">
-              <form
-                className="w-full max-w-md mx-auto p-6 bg-gray-800 text-white rounded shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
-                onSubmit={handleSubmit}
-              >
-                {error && (
-                  <div className="bg-slate-400 border-black border-4 text-black p-2 mb-2">
-                    {error}
+              {session?.user?.role === "administrador" ? (
+                <form
+                  ref={formRef}
+                  className="w-full max-w-md mx-auto p-6 bg-gray-800 text-white rounded shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105"
+                  onSubmit={handleSubmit}
+                >
+                  <h3 className="text-2xl text-white font-bold text-center mb-4">
+                    <u className="hover:underline hover:cursor-pointer transition duration-300 ease-in-out">
+                      Registrar Usuario
+                    </u>
+                  </h3>
+
+                  {error && (
+                    <div className="bg-red-500 text-white p-2 mb-2 rounded-md">
+                      {error}
+                    </div>
+                  )}
+
+                  <div className="mb-4">
+                    <label
+                      className="block text-sm font-semibold mb-2 text-gray-300"
+                      htmlFor="name"
+                    >
+                      Nombre completo
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="Nombre completo"
+                      className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                    />
                   </div>
-                )}
-                {successMessage && (
-                  <div className="bg-green-400 border-green-700 border-4 text-black p-2 mb-2">
-                    {successMessage}
+
+                  <div className="mb-4">
+                    <label
+                      className="block text-sm font-semibold mb-2 text-gray-300"
+                      htmlFor="email"
+                    >
+                      Correo electrónico
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Correo electrónico"
+                      className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                    />
                   </div>
-                )}
-                <h3 className="text-2xl text-white font-bold text-center mb-4">
-                  {" "}
-                  {/* Agrega margin-bottom aquí */}
-                  <u className="hover:underline hover:cursor-pointer transition duration-300 ease-in-out">
-                    Registrar Usuario
-                  </u>
-                </h3>
 
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="name"
-                  >
-                    Nombre completo
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Nombre completo"
-                    className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
-                  />
-                </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-sm font-semibold mb-2 text-gray-300"
+                      htmlFor="password"
+                    >
+                      Contraseña
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      placeholder="Contraseña"
+                      className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                    />
+                  </div>
 
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="email"
-                  >
-                    Correo electrónico
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Correo electrónico"
-                    className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
-                  />
-                </div>
+                  <div className="mb-4">
+                    <label
+                      className="block text-sm font-semibold mb-2 text-gray-300"
+                      htmlFor="role"
+                    >
+                      Rol
+                    </label>
+                    <select
+                      name="role"
+                      className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                    >
+                      <option value="consultor">Consultor</option>
+                      <option value="reponedor">Reponedor</option>
+                      <option value="administrador">Administrador</option>
+                    </select>
+                  </div>
 
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="password"
-                  >
-                    Contraseña
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Contraseña"
-                    className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
-                  />
+                  <div className="mb-6 flex space-x-4">
+                    <button
+                      className="bg-red-500 text-white px-2 py-2 flex-grow rounded hover:bg-red-600 focus:outline-none focus:ring focus:border-red-700"
+                      type="submit"
+                    >
+                      Registrar
+                    </button>
+                    <button
+                      className="bg-green-500 hover:bg-green-400 text-white font-semibold py-2 px-4 rounded border border-green-600 hover:border-green-400 focus:outline-none focus:shadow-outline ml-4"
+                      type="button"
+                      onClick={resetForm}
+                    >
+                      Limpiar Campos
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <div className="text-red-500 text-center">
+                  No tienes los permisos para registrar usuarios.
                 </div>
-
-                <div className="mb-4">
-                  <label
-                    className="block text-sm font-semibold mb-2 text-gray-300"
-                    htmlFor="role"
-                  >
-                    Rol
-                  </label>
-                  <select
-                    name="role"
-                    className="bg-white px-4 py-2 w-full text-gray-800 placeholder-gray-400 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
-                  >
-                    <option value="consultor">Consultor</option>
-                    <option value="reponedor">Reponedor</option>
-                    <option value="administrador">Administrador</option>
-                  </select>
-                </div>
-
-                <div className="mb-6">
-                  <button
-                    className="bg-red-500 text-white px-2 py-2 w-full rounded hover:bg-red-600 focus:outline-none focus:ring focus:border-red-700"
-                    type="submit"
-                  >
-                    Registrar
-                  </button>
-                </div>
-              </form>
+              )}
             </div>
           </div>
         </div>
       </div>
       <Footer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        progress={undefined}
+        style={{ fontSize: '1.4rem' }}
+      />
     </div>
   );
 }
